9b387ef21198b7bc761a540b2c02d16584ce5be8
Kesavan Venkatraman <Kesavan.Venkatraman@bglgroup.co.uk>
2017-10-03 14:08:35 +0100

    first angular app

---
+++ angtut/src/app/components/user/user.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent implements OnInit {
+name:string;
+
+  constructor() { 
+    console.log('constructor ran..');
+
+    this.name = 'me';
+  }
+
+  ngOnInit() {
+    console.log ('ngOnInit ran...')
+    
+    //this.name = 'K7';
+  }
+
+}
